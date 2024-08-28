a7114b465e343c95f2995e4a3350349287684c20
rafli <rafli21ti@mahasiswa.pcr.ac.id>
2024-08-29 00:22:08 +0700

    feat: data table with pagination

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,9 +1,14 @@
+import TableAdmin from '@/components/module/admin/TableAdmin'
+import { Card } from '@/components/ui/card'
 import React from 'react'
 
 export default function page() {
   return (
     <div className="flex w-full flex-col gap-4 p-6">
       <h2 className="text-2xl font-bold">Admin</h2>
+      <Card className="w-full">
+        <TableAdmin />
+      </Card>
     </div>
   )
 }
